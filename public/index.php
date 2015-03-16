2f37cd40d091aa277461ab3147c172960d5c7aa6
jeanineharb <jeanine.harb@gmail.com>
2015-03-16 14:08:43 +0200

    removed phpinfo()

--- public/index.php
+++ public/index.php
@@ -33,7 +33,6 @@ require __DIR__.'/../bootstrap/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-echo phpinfo();
 
 /*
 |--------------------------------------------------------------------------
