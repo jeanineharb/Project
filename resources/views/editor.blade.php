b32cfcec3e14d1f1e81b5fb20bd5a1ff945183a3
jeanineharb <jeanine.harb@gmail.com>
2015-04-14 09:20:18 +0300

    Revert "Removed CKEditor sample HTML pages."
    This reverts commit 4a3376a0b5008f9f4d84610510caffe44e79cbd7.

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -3,7 +3,9 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<form action="{{ asset('/ckeditor/samples/sample_posteddata.php')}}" method="post">
+
+				<form action="{{ asset('/ckeditor/samples/sample_posteddata.php')}}"
+					method="post">
 					<textarea name="editor1" id="editor1" rows="10" cols="80">
                 		This is my textarea to be replaced with CKEditor.
             		</textarea>
@@ -61,6 +63,7 @@
 		                });
             		</script>
 					<p> <input type="submit" value="Submit"> </p>
+
 				</form>
 			</div>
 		</div>
