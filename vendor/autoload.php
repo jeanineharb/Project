48f5eebbaebb76d3511b17175aa11f69096648cb
jeanineharb <jeanine.harb@gmail.com>
2015-04-28 09:40:48 +0300

    Updated composer.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit466175449baff3e13c91fb38b9845218::getLoader();
+return ComposerAutoloaderInit4964fc7ccad6e91a1c1ac0611b45436d::getLoader();
