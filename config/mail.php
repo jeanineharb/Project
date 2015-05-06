d8030c521cad0d015fe129e11a1dcbfd4e31b5a1
MansourHachem <mansour.hachem@hotmail.com>
2015-05-06 13:38:49 +0300

    mail

--- config/mail.php
+++ config/mail.php
@@ -4,17 +4,16 @@ return [
 
     'driver' => 'smtp',
  
-    'host' => 'smtp.gmail.com',
+    'host' => 'sub4.mail.dreamhost.com',
  
     'port' => 587,
  
-    'from' => array('address' => 'humanressources@bankaudi.com', 'name' => 'Bank Audi'),
  
     'encryption' => 'tls',
  
-    'username' => 'hachemmansour21@gmail.com',
+    'username' => 'info1@templ8.dreamhosters.com',
  
-    'password' => 'm',
+    'password' => 'testtest12',
  
     'sendmail' => '/usr/sbin/sendmail -bs',
  
