2ed049443b61b2627bd4584d9ccdbc75a9d48ddc
jeanineharb <jeanine.harb@gmail.com>
2015-05-07 19:14:05 +0300

    Changed javascript function in Template view.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -14,8 +14,11 @@
   	</h1>
   </div>
   <div class="col-xs-6 col-md-4" style="text-align: right;">
+  <form action="{{ asset('/ckeditor/samples/sample_posteddata.php')}}"
+					method="post">
   	<button type="button" class="btn btn-default">Discard</button>
-  	<button type="button" class="btn btn-default">Save</button>
+  	<button type="button" class="btn btn-default" onclick="onSubmit()">Save</button>
+  	</form>
   </div>
 	
 </div>
@@ -24,7 +27,31 @@
 $temp = Template::find(1)->pluck('html');
 echo $temp;
 ?>
+
 <script src="{{ asset('/ckeditor/ckeditor.js') }}"></script>
 <script src="{{ asset('/ckeditor/inlineEditorWithCustomButton.js') }}"></script>
+
+<script>
+function redirect(redirectUrl, arg, value) {
+	  var form = $('<form action="' + redirectUrl + '" method="post"' +
+	  '<input type="hidden" name="'+ arg +'" value="' + value + '"></input>' + '</form>');
+	  $('body').append(form);
+	  $(form).submit();
+}
+	
+function onSubmit(){
+		var data = "";
+		
+		for (var i in CKEDITOR.instances){
+			data += CKEDITOR.instances[i].getData();
+		}
+
+		console.log(data);
+
+ 		redirect("{{ asset('/ckeditor/samples/sample_posteddata.php')}}", "variable", data);
+}
+
+
+</script>
 					
 @endsection
