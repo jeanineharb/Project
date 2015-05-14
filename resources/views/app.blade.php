a0c0619316583468b317bdb903026a9e5fe9843b
jeanineharb <jeanine.harb@gmail.com>
2015-05-14 15:27:12 +0300

    Added Create Template tab.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,6 +45,7 @@
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
+						<li><a href="{{ url('/editor') }}">Create Template</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
