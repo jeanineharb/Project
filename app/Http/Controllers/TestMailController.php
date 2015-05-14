cdf1127d5a46b279e1c7e32f6de0c179cdbf9821
MansourHachem <mansour.hachem@hotmail.com>
2015-05-14 13:51:01 +0300

    mail

--- app/Http/Controllers/TestMailController.php
+++ app/Http/Controllers/TestMailController.php
@@ -32,7 +32,7 @@ class TestMailController extends Controller {
 	 */
 	public function index()
 	{
-		echo "asdads";
+		echo "asdadse2";
 
 	$content = "Hi,welcome user!";
 
@@ -43,9 +43,9 @@ class TestMailController extends Controller {
 
 	Mail::send('emails.mail-template', $data, function($message) {
 
-    $message->to('jeanine.harb@gmail.com', 'Jon Doe');
+    $message->to('mansour.hachem@.com', 'Jon Doe');
     $message->subject('Welcome to the Laravel 4 Auth App!');
-    $message->from('test@audi.com', 'Robert Traboulsi');
+    $message->from('kjhkjh@k.com', 'lm lm');
 
 });
 
