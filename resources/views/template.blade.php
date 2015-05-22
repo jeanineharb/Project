f4be3f4e23bb80c947bf4bae44d759df6b0f9c14
jeanineharb <jeanine.harb@gmail.com>
2015-05-22 11:17:15 +0300

    Added confirmation dialog for discard button and modified save function

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,7 +8,7 @@
   	<h1> {{ $temp->templateName }} </h1>
   </div>
   <div class="col-xs-6 col-md-4" style="text-align: right;">
-  	<button type="button" class="btn btn-default">Discard</button>
+  	<button type="button" class="btn btn-default" onclick="discard()">Discard</button>
   	<button type="button" class="btn btn-default" onclick="onSubmit()">Save</button>
   </div>
 </div>
@@ -20,6 +20,8 @@ echo $temp->css;
 
 <script src="{{ asset('/ckeditor/ckeditor.js') }}"></script>
 <script src="{{ asset('/ckeditor/inlineEditorWithCustomButton.js') }}"></script>
+<script src="{{ asset('/bootbox.min.js') }}"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
 <script>
 function redirect(redirectUrl, arg, value) {
@@ -36,11 +38,30 @@ function onSubmit(){
 			data += CKEDITOR.instances[i].getData();
 		}
 
-		console.log(data);
+		//console.log(data);
 
- 		redirect("{{ asset('/ckeditor/samples/sample_posteddata.php')}}", "variable", data);
+		var x = document.getElementsByTagName("field");
+		var i;
+		var att=[];
+		for (i = 0; i < x.length; i++) {
+			att[i]=x.item(i).attributes.getNamedItem("id").value;
+			console.log(att[i]);
+		}
+
+ 		//redirect("{{ asset('/ckeditor/samples/sample_posteddata.php')}}", "variable", data);
 }
 
+
+function discard(){
+	bootbox.confirm("Are you sure you want to discard your changes?", function(result) {
+		if(result) {
+            top.location.href = "{{ url('/template') }}";
+        }
+        else{
+        	bootbox.hideAll();
+        }
+	}); 
+}
 </script>
 
 
