6fb6dfacba0fe3650f5aef0bdafba5749ee4344b
jeanineharb <jeanine.harb@gmail.com>
2015-05-24 21:24:56 +0300

    Corrected Discard behaviour

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,11 +8,9 @@
 			<h1> {{ $temp->templateName }} </h1>
 		</div>
 		<div class="col-xs-6 col-md-4" style="text-align: right;">
-			<?php echo Form::open(['id' => 'tempForm']); ?>
-
- 			<button type="button" class="btn btn-default" onclick="discard()">Discard</button>
-
-			<?php echo Form::submit('Save', ['class' => 'btn btn-primary', 'id' => 'save', 'style' => 'margin-right: 3px;']); 
+			<button type="button" class="btn btn-default" onclick="discard()">Discard</button>
+			<?php echo Form::open(['id' => 'tempForm']);
+				  echo Form::submit('Save', ['class' => 'btn btn-primary', 'id' => 'save', 'style' => 'margin-right: 3px;']); 
 				  echo Form::submit('Send', ['class' => 'btn btn-success', 'id' => 'send']); 
 				  echo Form::close(); ?>
 		</div>
